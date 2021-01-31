dbadfb5a6626f9f502d897594eb12b6bda335dcd
antonymss <demeshkevichanton@gmail.com>
2021-01-31 12:56:25 +0300

    create new counter

---
+++ src/butt.tsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+//
+// type buttonsType = {
+//     name: string
+//     isDisabled: boolean
+//     callBack: () => void
+// }
+//
+// export let Buttons = (props:buttonsType) => {
+//
+//     return(
+// <button onClick={()=>{props.callBack()}} disabled={props.isDisabled}>
+//     {props.name}
+// </button>
+//     )
+//
+// }
